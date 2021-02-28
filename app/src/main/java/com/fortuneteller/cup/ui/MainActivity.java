edf59fa6ad609f3b980307216113d492c889691e
waeladel <blackhorse_134@hotmail.com>
2021-03-01 00:15:45 +0200

    Toolbar instead of actionBar

--- app/src/main/java/com/fortuneteller/cup/ui/MainActivity.java
+++ app/src/main/java/com/fortuneteller/cup/ui/MainActivity.java
@@ -3,27 +3,38 @@ package com.fortuneteller.cup.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.fortuneteller.cup.R;
+import com.fortuneteller.cup.databinding.ActivityMainBinding;
+import com.fortuneteller.cup.databinding.ToolbarBinding;
 
 //import com.fortuneteller.cup.databinding.ActivityainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController ;
-    //private ActivityainBinding mBinding;
+    private ActivityMainBinding mBinding;
+    private ToolbarBinding mToolbarBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //mBinding = ActivityainBinding.inflate(getLayoutInflater());
-        //View view = mBinding.getRoot();
-        //setContentView(view);
+        //setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        mToolbarBinding = mBinding.toolbar;
+        View view = mBinding.getRoot();
+        setContentView(view);
+        //setSupportActionBar(mBinding.toolbar);
 
         navController = Navigation.findNavController(this, R.id.host_fragment);
+
+        // Setup toolbar
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(mToolbarBinding.toolbar, navController);
 
         //goToMainFragment(); // No need it's already the home distination
     }
